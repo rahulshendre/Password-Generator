af4456d6aea68b87f506b1212fb9b445f78d80a5
Rahul <rahulshendre789@gmail.com>
2024-08-11 11:13:49 +0530

    new project created

---
+++ script.js
@@ -0,0 +1,32 @@
+const passwordBox = document.getElementById("password");
+
+const length = 10; 
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789"
+const symbol = "# $ % ^ & * ( ) _ - + = < > ? / | @ ! ~ { } [ ] : ;"
+
+
+const allChars = upperCase + lowerCase + number + symbol; 
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password += number[Math.floor(Math.random() * number.length)]
+    password += symbol[Math.floor(Math.random() * symbol.length)]
+    while (password.length < 10){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+    passwordBox.value = password;
+    }
+    function copyPassword(){
+       navigator.clipboard.writeText(passwordBox.value)
+       .then(()=>{
+        alert("Password copied to clipboard")
+       })
+       .catch(err =>{
+        alert("Error");
+       })
+
+}   
